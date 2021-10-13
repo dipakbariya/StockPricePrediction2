67faaad8a9b1098a9d83d7ba2ea66172a66ef2c8
dipakbariya <63631974+dipakbariya@users.noreply.github.com>
2021-10-13 13:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -447,7 +447,7 @@ def predict():
 
     
     pred = model(df, date)
-    return render_template('index.html', prediction_text='Predicted Close Price is $ {}'.format(round(pred[0][0],2)))
+    return render_template('index.html', prediction_text='Predicted Close Price is $ {}'.format(round(pred[0][0],2))) 
 
 
 if __name__ == "__main__":
